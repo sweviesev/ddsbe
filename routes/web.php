8c1617ab679867878ec80b7be24089de6eb898b4
sweviesev <raut.kirstian@gmail.com>
2025-03-24 21:13:18 +0800

    update2

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ $router->get('/', function () use ($router) {
 });
 
 $router->group(['prefix' => 'api'], function () use ($router) {
-    $router->get('/users', 'UserController@getUsers'); // Get all users
-    $router->post('/users', 'UserController@add'); // Create user
-    $router->get('/users/{id}', 'UserController@show'); // Get user by ID
-    $router->put('/users/{id}', 'UserController@update'); // Update user
-    $router->delete('/users/{id}', 'UserController@delete'); // Delete user
+    $router->get('/users', 'UserController1@getUsers'); // Get all users
+    $router->post('/users', 'UserController1@add'); // Create user
+    $router->get('/users/{id}', 'UserController1@show'); // Get user by ID
+    $router->put('/users/{id}', 'UserController1@update'); // Update user
+    $router->delete('/users/{id}', 'UserController1@delete'); // Delete user
 });
