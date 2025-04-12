21f2707f0eb9c7626ff99f01aa1d529092bb738a
sweviesev <raut.kirstian@gmail.com>
2025-04-12 15:57:35 +0800

    timestamp

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model {
     protected $table = 'tbluser';
     protected $fillable = ['username', 'password', 'gender', 'jobid'];
+    public $timestamps = false;
     public function job()
     {
         return $this->belongsTo(UserJob::class, 'jobid'); // Reference jobid in tbluserjob
